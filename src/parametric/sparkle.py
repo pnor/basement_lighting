49abab557cb46937de9dacdf75565db91c3e1802
Rasberry pi in the basement <pnoreggio@gmail.com>
2022-12-13 22:29:54 -0500

    sparkle fix

--- src/parametric/sparkle.py
+++ src/parametric/sparkle.py
@@ -26,22 +26,25 @@ ceil = Ceiling(auto_write=False)
 ceil.use_linear()
 ceil.clear()
 
-colors = color_range(color_input, dim_color(color_input), 100)
+colors = color_range(dim_color(color_input), color_input, 100)
+colors = colors + list(reversed(colors))
 
 FPS = 30
 DELTA = 1 / FPS
 
 progress_indeces = (((np.arange(100) / 99) ** 10) * 99).astype(int)
-progresses = np.random.random_integers(0, 99, ceil.NUMBER_LIGHTS)
+progress_indeces = list(progress_indeces) + list(reversed(progress_indeces))
+progresses = np.random.random_integers(0, 199, ceil.NUMBER_LIGHTS)
 
 cur_time = 0
 
 while True:
     cur_time += DELTA
-    if cur_time > interval:
+    if cur_time > (interval / 200):
         cur_time = 0
-        progresses = (progresses + 1) % 100
+        progresses = (progresses + 1) % 200
         for i in range(len(progresses)):
             ceil[i] = colors[progress_indeces[progresses[i]]]
+
         ceil.show()
     time.sleep(DELTA)
--- sudo_script.sh
+++ sudo_script.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
-python ./src/parametric/fade.py '#ff0' 1
+# python ./src/parametric/fade.py '#ff0' 1
+
+python ./src/parametric/sparkle.py '#ff0' 1
 
 # python ./src/parametric/runthrough.py '#aaa' 3
 
-# python ./src/parametric/fill.py '#000'
+python ./src/parametric/fill.py '#000'

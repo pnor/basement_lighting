03d52d139131ecdc16c82ae683d34542db1b5a18
Jared Tulayan <jared@jaredtulayan.xyz>
2022-12-14 18:25:34 -0500

    Start setting up flask

--- src/package/app.py
+++ src/package/app.py
@@ -1,7 +1,11 @@
-import os
-
 from flask import Flask
+import os
 from package.blueprints import root
+import sass
+import sassutils
+from sassutils.wsgi import SassMiddleware
+
+
 
 
 def create_app(test_config=None):
@@ -10,6 +14,10 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
     )
+
+    app.wsgi_app = SassMiddleware(app.wsgi_app, {
+        'package': ('static/sass', 'static/css', '/static/css')
+    })
 
     if test_config is None:
         # load the instance config, if it exists, when not testing

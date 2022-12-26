5419a835778685b645dbf0de01ad52d75938c396
Phillip O'Reggio <pnoreggio@gmail.com>
2022-12-26 17:34:48 -0500

    move all scripts to new render

--- scripts/light_scripts/sparkle.py
+++ scripts/light_scripts/sparkle.py
@@ -17,24 +17,34 @@ from scripts.library.render import RenderState
 
 class Render(RenderState):
     def __init__(self, color: RGB, number_lights: int, interval: Optional[float]):
-        self.progresses = np.random.randint(0, 199 + 1, number_lights)
-        self.progress_indeces = (((np.arange(100) / 99) ** 10) * 99).astype(int)
+        interval = interval if interval else 1
+        # progress for each LED in color cycle
+        # 0..199
+        self.progresses = np.random.randint(0, number_lights, number_lights)
+
+        # Indeces used to index into colors
+        # doesn't cover every index to make the fade effect
+        all_steps = number_lights // 2
+        self.progress_indeces = (
+            ((np.arange(all_steps) / (all_steps - 1)) ** 10) * (all_steps - 1)
+        ).astype(int)
         self.progress_indeces = list(self.progress_indeces) + list(
             reversed(self.progress_indeces)
         )
 
-        self.colors = color_range(dim_color(color), color, 100)
+        # All colors in a range, indexed into using progress indeces
+        self.colors = color_range(dim_color(color), color, all_steps)
         self.colors = self.colors + list(reversed(self.colors))
 
-        super().__init__(interval)
+        super().__init__(interval * 7)
 
     def render(self, delta: float, ceil: Ceiling) -> Union[bool, None]:
-        # TODO redo how we find the colors
-        index = int(self.progress() * len(self.colors))
+        index = int(self.progress() * len(self.progress_indeces))
         for i in range(len(self.progresses)):
-            progress_index = self.progress_indeces[self.progresses[i]]
-            progress_index = (progress_index + index) % len(self.colors)
-            ceil[i] = self.colors[progress_index]
+            prog_index = (index + self.progresses[i]) % len(self.progress_indeces)
+            color_index = self.progress_indeces[prog_index]
+            ceil[i] = self.colors[color_index]
+
         ceil.show()
 
 
@@ -47,30 +57,12 @@ def run(**kwargs):
     ceil.clear()
 
     render_loop = Render(color_input, ceil.NUMBER_LIGHTS, interval)
-    render_loop.run(60, ceil)
-    # colors = color_range(dim_color(color_input), color_input, 100)
-    # colors = colors + list(reversed(colors))
-
-    # FPS = 60
-    # DELTA = 1 / FPS
-
-    # progress_indeces = (((np.arange(100) / 99) ** 10) * 99).astype(int)
-    # progress_indeces = list(progress_indeces) + list(reversed(progress_indeces))
-    # progresses = np.random.randint(0, 199 + 1, ceil.NUMBER_LIGHTS)
-
-    # cur_time = 0
-
-    # while True:
-    #     cur_time += DELTA
-    #     if cur_time > (interval / 200):
-    #         cur_time = 0
-    #         progresses = (progresses + 1) % 200
-    #         for i in range(len(progresses)):
-    #             ceil[i] = colors[progress_indeces[progresses[i]]]
-
-    #         ceil.show()
-    #     time.sleep(DELTA)
+    render_loop.run(30, ceil)
 
 
 if __name__ == "__main__":
-    run(ceiling=Ceiling(test_mode=True), color=sys.argv[1], interval=sys.argv[2])
+    run(
+        ceiling=Ceiling(),
+        color=sys.argv[1],
+        interval=sys.argv[2],
+    )
--- scripts/light_scripts/spinning.py
+++ scripts/light_scripts/spinning.py
@@ -5,36 +5,42 @@
 
 import sys
 import time
+from typing import Optional, Union
+from backend.backend_types import RGB
 
 from backend.ceiling import Ceiling
 from backend.util import color_range, dim_color
+from scripts.library.render import RenderState
+
+
+class Render(RenderState):
+    def __init__(self, color: RGB, interval: Optional[float]):
+        # Getting range of colors for all LEDs to cycle through
+        self.TAIL_LENGTH = 8
+        self.colors = color_range(color, dim_color(color), self.TAIL_LENGTH)
+
+        super().__init__(interval)
+
+    def render(self, delta: float, ceil: Ceiling) -> Union[bool, None]:
+        theta = self.progress() * 360
+
+        for i in range(0, self.TAIL_LENGTH):
+            ceil[0.6, theta - (i * 20)] = self.colors[i]
+
+        ceil.show()
+        return super().render(delta, ceil)
 
 
 def run(**kwargs):
     color_input = kwargs["color"]
     interval = float(kwargs["interval"])
 
-    ceil = kwargs["ceiling"]
+    ceil: Ceiling = kwargs["ceiling"]
     ceil.use_polar((0.5, 0.5))
     ceil.clear()
 
-    # Getting range of colors for all LEDs to cycle through
-    TAIL_LENGTH = 8
-    colors = color_range(color_input, dim_color(color_input), TAIL_LENGTH)
-
-    FPS = 60
-    DELTA = 1 / FPS
-    cur_time = 0
-    while True:
-        cur_time = (cur_time + DELTA) % interval
-        theta = (cur_time / interval) * 360
-
-        ceil.clear(False)
-        for i in range(0, TAIL_LENGTH):
-            ceil[0.6, theta - (i * 20)] = colors[i]
-        ceil.show()
-
-        time.sleep(DELTA)
+    render_loop = Render(color_input, interval)
+    render_loop.run(30, ceil)
 
 
 if __name__ == "__main__":

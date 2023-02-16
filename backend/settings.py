3ca0db06b51317994956650c79298d2a34aa4981
Phillip O'Reggio <pnoreggio@gmail.com>
2023-02-16 10:35:19 -0500

    make dimensions and number lights optional in settings

--- backend/settings.py
+++ backend/settings.py
@@ -4,8 +4,9 @@
 Parses configs from a toml file
 """
 
-from typing import List, Optional
+from typing import List, Optional, Tuple
 import toml
+import csv
 
 
 class Settings:
@@ -14,10 +15,18 @@ class Settings:
 
         self.url: str = dict["settings"]["url"]
 
-        self.number_lights: int = dict["settings"]["number_lights"]
         self.arrangement_file: str = dict["settings"]["arrangement_file"]
+        dimensions, number_lights = preprocess_arrangement_file(self.arrangement_file)
+
+        num_lights = dict["settings"].get("number_lights")
+        self.number_lights: int = (
+            num_lights if num_lights is not None else number_lights
+        )
+
+        dims = dict["settings"].get("dimensions")
+        self.dimensions: int = dims if dims is not None else dimensions
+
         self.io_pin: int = dict["settings"]["io_pin"]
-        self.dimensions: int = dict["settings"]["dimensions"]
         self.rows: Optional[List[int]] = dict["settings"].get("rows")
 
         self.sphere_size: Optional[float] = dict["test"].get("sphere_size")
@@ -28,3 +37,24 @@ class Settings:
 
         test_mode = dict["test"].get("test_mode")
         self.test_mode: bool = False if test_mode is None else bool(test_mode)
+
+
+def preprocess_arrangement_file(file: str) -> Tuple[int, int]:
+    """
+    Infers the number of lights and total number of dimensions from the arrangement csv file
+
+    Witht this, you acn omit number_lights and dimensions
+    """
+    num_lines = sum(1 for line in open(file) if line.strip() != "")
+
+    first_row = True
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if first_row:
+                first_row = False
+            else:
+                dimensions = len(row) - 1
+                return dimensions, num_lines - 1
+
+    raise Exception("Failed to preprocess arrangement file!")
